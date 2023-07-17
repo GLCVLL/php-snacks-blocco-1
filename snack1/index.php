2b709ddbea815e5cdbaf128f39ffbf6e59ea4d87
GLCVLL <g.ioele96@outlook.it>
2023-07-17 16:46:45 +0200

    made games array

---
+++ snack1/index.php
@@ -0,0 +1,21 @@
+<?php
+
+// Creazione dell'array delle partite
+$games = [
+    ['squadra_casa' => 'Olimpia Milano',
+     'squadra_ospite' => 'Cantù',
+     'punti_casa' => 55,
+     'punti_ospite' => 60],
+
+    ['squadra_casa' => 'Roma',
+     'squadra_ospite' => 'Napoli',
+     'punti_casa' => 70,
+     'punti_ospite' => 68],
+
+    ['squadra_casa' => 'Bologna',
+     'squadra_ospite' => 'Venezia',
+     'punti_casa' => 82,
+     'punti_ospite' => 78]
+];
+
+?>
