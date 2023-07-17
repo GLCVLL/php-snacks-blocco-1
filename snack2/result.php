94c8c5ef4380ce76437264097110d7ce06965c90
GLCVLL <g.ioele96@outlook.it>
2023-07-17 17:22:05 +0200

    checked all conditions

--- snack2/result.php
+++ snack2/result.php
@@ -5,4 +5,12 @@ $name = $_GET['name'] ?? '';
 $mail = $_GET['mail'] ?? '';
 $age = $_GET['age'] ?? '';
 
+
+// Verifico se le condizioni sono esatte o meno
+
+if (strlen($name) > 3 && strpos($mail, '.') !== false && strpos($mail, '@') !== false && is_numeric($age)) {
+    echo "Accesso riuscito";
+} else {
+    echo "Accesso negato";
+}
 ?>
